df030cc8048032fc7bbed94193f72c2b981ff22b
CeciTeser <mcteser@gmai.com>
2021-05-07 00:52:59 +0200

    Excercise Scroll - Events

---
+++ Module-2-JavaScript/DOM/DOM-Eventos/Scroll/script.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('#body');
+body.style.height=`2500px`
+
+const cambiaColor =()=>{
+    if(0<window.scrollY && window.scrollY<=500) {
+        body.style.backgroundColor="red"
+    } 
+    else if(501<window.scrollY && window.scrollY<=1000) {
+        body.style.backgroundColor="grey"
+    } 
+    else if(1001<window.scrollY && window.scrollY<=1500) {
+        body.style.backgroundColor="green"
+    } 
+    else if(1501<window.scrollY && window.scrollY<=2000) {
+        body.style.backgroundColor="orange"
+    }
+}
+
+window.addEventListener('scroll', cambiaColor);
