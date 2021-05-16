7c2e0f4f6c2f4046560f3ebf3646f75fb506bc75
CeciTeser <mcteser@gmai.com>
2021-05-16 20:40:56 +0200

    "Excercise swith tamagochi"

--- Module-2-JavaScript/DOM/DOM-Eventos-Formulario/Agregar imágenes/scriptimg.js
+++ Module-2-JavaScript/DOM/DOM-Eventos-Formulario/Agregar imágenes/scriptimg.js
@@ -1,3 +1,4 @@
+
 const containerImg= document.querySelector('#container');
 containerImg.style=`width:1000px; height: 1000px; border:1px solid #DDD;`;
 
@@ -12,7 +13,7 @@ const imgInput=document.querySelector('#img-input');
 function showImg(){
     let urlAdd = imgInput.value
     const imgAdd = document.querySelector('#img');
-    imgAdd.style=`width:100%; height: 100%; object-fit: contain;background-position:center;background-size: 100%;`;
+    imgAdd.style=`width:100%; height: 100%;object-fit:contain;background-position:center;background-size: 100%;`;
     imgAdd.style.backgroundImage=`url("${urlAdd}")`;
     document.getElementById('img-input').value = "";
 }
