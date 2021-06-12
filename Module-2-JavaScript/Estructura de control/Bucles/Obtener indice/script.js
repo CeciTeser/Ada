440814ad07834cf6221c4c99aae417c4a24dc713
CeciTeser <mcteser@gmai.com>
2021-06-12 21:22:13 +0200

    Practice Estructura de control:Bucles

---
+++ Module-2-JavaScript/Estructura de control/Bucles/Obtener indice/script.js
@@ -0,0 +1,14 @@
+const array =[5, 7, 12, 34, 54, 2, 12];
+let valor = 83;
+console.log(array);
+
+
+const obtenerIndice = (array) =>{
+
+    let output = array.indexOf(valor);
+        console.log(output)
+
+};
+
+
+obtenerIndice(array);
